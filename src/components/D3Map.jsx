0f8b67cedf867e31ca06c199eb274c89ba9eb011
priyansh agrahari <75441026+r3dacted42@users.noreply.github.com>
2024-12-14 21:52:00 +0530

    zoom works

--- src/components/D3Map.jsx
+++ src/components/D3Map.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import nodeData from '../data.json';
 import * as d3 from 'd3';
 import './D3Map.css';
@@ -6,54 +6,94 @@ import { useTooltip } from './Tooltip';
 
 export default function D3Map() {
     const svgRef = useRef(null);
+    const containerRef = useRef(null);
+    const gridRef = useRef(null);
     const [tooltip, setTooltipVisible, setTooltipData, setPageXY] = useTooltip();
 
-    const [width, setWidth] = useState(null);
-    const [height, setHeight] = useState(null);
-
-    const [nodes, setNodes] = useState(null);
-    const [xScale, setXScale] = useState(null);
-    const [yScale, setYScale] = useState(null);
-    const [transform, setTransform] = useState(null);
-
-    function handleResize() {
-        setWidth(svgRef.current.clientWidth);
-        setHeight(svgRef.current.clientHeight);
-    }
-
     useEffect(() => {
-        const _width = svgRef.current.clientWidth;
-        const _height = svgRef.current.clientHeight;
-        window.addEventListener("resize", handleResize);
-
         const svg = d3.select(svgRef.current);
-        svg.attr("width", _width).attr("height", _height);
+        const container = d3.select(containerRef.current);
+        const gridGroup = d3.select(gridRef.current);
 
-        const _xScale = d3.scaleLinear()
+        // Get the actual dimensions of the SVG
+        const width = svg.node().clientWidth;
+        const height = svg.node().clientHeight;
+
+        // Initial scales
+        const xScale = d3.scaleLinear()
             .domain([d3.min(nodeData, d => d.x), d3.max(nodeData, d => d.x)])
-            .range([50, _width - 50]);
-        setXScale(_xScale);
+            .range([50, width - 50]);
 
-        const _yScale = d3.scaleLinear()
+        const yScale = d3.scaleLinear()
             .domain([d3.min(nodeData, d => d.y), d3.max(nodeData, d => d.y)])
-            .range([50, _height - 50]);
-        setYScale(_yScale);
+            .range([50, height - 50]);
 
+        // Function to draw gridlines
+        function drawGridlines(xScale, yScale) {
+            // Clear previous gridlines completely
+            gridGroup.selectAll("*").remove();
+
+            // Vertical gridlines
+            gridGroup.selectAll("line.vertical")
+                .data(xScale.ticks(10))
+                .enter()
+                .append("line")
+                .attr("class", "vertical")
+                .attr("x1", d => xScale(d))
+                .attr("x2", d => xScale(d))
+                .attr("y1", 0)
+                .attr("y2", height)
+                .attr("stroke-dasharray", "2,2");
+
+            // Horizontal gridlines
+            gridGroup.selectAll("line.horizontal")
+                .data(yScale.ticks(10))
+                .enter()
+                .append("line")
+                .attr("class", "horizontal")
+                .attr("x1", 0)
+                .attr("x2", width)
+                .attr("y1", d => yScale(d))
+                .attr("y2", d => yScale(d))
+                .attr("stroke-dasharray", "2,2");
+        }
+
+        // Initial gridlines
+        drawGridlines(xScale, yScale);
+
+        // Create zoom behavior with constrained panning
         const zoom = d3.zoom()
             .scaleExtent([0.5, 4])
-            .on("zoom", zoomed);
+            .translateExtent([
+                [-width, -height],     // Top-left boundary
+                [width * 2, height * 2] // Bottom-right boundary
+            ])
+            .on("zoom", (event) => {
+                // Apply the zoom transformation to the container
+                container.attr("transform", event.transform);
+
+                // Update gridlines with new scales
+                const newXScale = event.transform.rescaleX(xScale);
+                const newYScale = event.transform.rescaleY(yScale);
+                drawGridlines(newXScale, newYScale);
+            });
+
+        // Apply zoom to the svg
         svg.call(zoom);
 
-        let _nodes = svg.selectAll(".node")
+        // Render nodes
+        const nodes = container.selectAll(".node")
             .data(nodeData)
             .enter()
             .append("g")
-            .attr("transform", d => `translate(${_xScale(d.x)}, ${_yScale(d.y)})`)
+            .attr("class", "node")
+            .attr("transform", d => `translate(${xScale(d.x)}, ${yScale(d.y)})`)
             .each(function (d) {
                 d3.select(this)
                     .append("circle")
-                    .attr("class", "node")
+                    .attr("class", "node-circle")
                     .attr("r", 16);
+                
                 d3.select(this)
                     .append("use")
                     .attr("xlink:href", `#${d.type}`)
@@ -64,77 +104,61 @@ export default function D3Map() {
                     .attr("y", -12);
             });
 
-        function handleTooltipMove(event) {
-            setPageXY({
-                x: event.pageX,
-                y: event.pageY
-            });
-        }
+        // Tooltip interactions
+        nodes.on("mouseover", (event, d) => {
+            const currentNode = d3.select(event.currentTarget);
+            
+            // Store original transform
+            const originalTransform = currentNode.attr("transform");
+            currentNode.attr("data-original-transform", originalTransform);
 
-        _nodes.on("mouseover", (event, d) => {
-            // Increase node size on hover
-            d3.select(event.target)
-                .on("mousemove", handleTooltipMove)
+            // Scale up node
+            currentNode
                 .transition()
                 .duration(150)
-                .attr("transform", "scale(1.2)");
+                .attr("transform", `${originalTransform} scale(1.2)`);
 
             // Show tooltip
             setTooltipVisible(true);
             setTooltipData(d);
-            handleTooltipMove(event);
-        })
-            .on("mouseout", (event, d) => {
-                // Reset node size on mouseout
-                d3.select(event.target)
-                    .on("mousemove", null)
-                    .transition()
-                    .duration(150)
-                    .attr("transform", "scale(1)");
-
-                // Hide tooltip
-                setTooltipVisible(false);
+            setPageXY({
+                x: event.pageX,
+                y: event.pageY
             });
+        })
+        .on("mousemove", (event) => {
+            // Update tooltip position without moving the node
+            setPageXY({
+                x: event.pageX,
+                y: event.pageY
+            });
+        })
+        .on("mouseout", (event, d) => {
+            const currentNode = d3.select(event.currentTarget);
+            
+            // Restore original transform
+            const originalTransform = currentNode.attr("data-original-transform");
+            currentNode
+                .transition()
+                .duration(150)
+                .attr("transform", originalTransform);
 
-        setNodes(_nodes);
-
-        function zoomed({ transform }) {
-            console.log("zooming...");
-            setTransform(transform);
-        }
-
-        return (() => {
-            window.removeEventListener("resize", handleResize);
+            // Hide tooltip
+            setTooltipVisible(false);
         });
-
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
-
-    useEffect(() => {
-        const svg = d3.select(svgRef.current);
-        svg.attr("width", width).attr("height", height);
-        setXScale(d3.scaleLinear()
-            .domain([d3.min(nodeData, d => d.x), d3.max(nodeData, d => d.x)])
-            .range([50, width - 50]));
-        setYScale(d3.scaleLinear()
-            .domain([d3.min(nodeData, d => d.y), d3.max(nodeData, d => d.y)])
-            .range([50, height - 50]));
-    }, [height, width]);
-
-    useEffect(() => {
-        if (!transform || !xScale || !yScale) return;
-        
-        var new_xScale = transform.rescaleX(xScale);
-        var new_yScale = transform.rescaleY(yScale);
-        setXScale(new_xScale);
-        setYScale(new_yScale);
-        nodes.attr('transform', d => `translate(${new_xScale(d.x)}, ${new_yScale(d.y)}) scale(${transform.k})`);
-        setNodes(nodes);
-    }, [transform]);
 
     return (
         <>
-            <svg className='map' ref={svgRef}></svg>
+            <svg 
+                ref={svgRef} 
+                className='map'
+            >
+                <g ref={gridRef} className="grid"></g>
+                <g ref={containerRef}>
+                    {/* Nodes will be added here by D3 */}
+                </g>
+            </svg>        
             {tooltip}
         </>
     );
